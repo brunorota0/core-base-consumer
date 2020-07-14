d803e112a63a5b16bb1b9660cc8f716142463edc
Bruno Rota <brota001@soa.ad.pwcinternal.com>
2020-07-14 11:26:57 -0300

    New r function implementation

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
-import { test, createElement, updateNode, removeNode, createNode } from 'robotjscorebase'
+import { test, r, updateNode, removeNode, createNode, render, VirtualNode } from 'robotjscorebase'
 
-test('test text');
+// test('test text');
 
-const element = createElement({
-    key: 'test',
-    element: 'div',
-    selector: 'body div',
-    text: 'test text',
-    classes: ['testclass1', 'testclass2'],
-    attrs: [
-        { testAttrKey: 'testAttrValue' },
-        { testAttrKey2: 'testAttrValue2' }
-    ],
-    children: [],
-    style: 'color: red'
-})
+// const element = createElement({
+//     key: 'test',
+//     element: 'div',
+//     selector: 'body div',
+//     text: 'test text',
+//     classes: ['testclass1', 'testclass2'],
+//     attrs: [
+//         { testAttrKey: 'testAttrValue' },
+//         { testAttrKey2: 'testAttrValue2' }
+//     ],
+//     children: [],
+//     style: 'color: red'
+// })
 
-console.log(element);
+// console.log(element);
+
+const component = r("div", [], [
+    r("button", [], [], undefined, undefined, "Button text"),
+    r("h1", [], [])
+])
+
+render(component, 'root')
